5bb9f46247014694a386de8b79da5d5a581480fe
Milan Hauth <milahu@gmail.com>
2022-12-10 11:54:09 +0100

    elements.html to elements.ts

---
+++ app/elements/elements.ts
@@ -0,0 +1,7 @@
+// Iron elements
+import '@polymer/iron-flex-layout/iron-flex-layout.js'
+
+// Paper elements
+import '@polymer/paper-material/paper-material.js'
+
+import "./browsix-terminal/browsix-terminal"
